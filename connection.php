a5cd30e85ffb7a86c68eb189161506dab3ab3a3f
Giau <giautngcc19047@fpt.edu.vn>
2021-11-08 14:29:22 +0700

    update2

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = pg_connect("postgres://sldjryctmwqljp:344b41c89cf2851b75d67a9936fce5529a3195de3f5663a5899699d419d70546@ec2-54-226-56-198.compute-1.amazonaws.com:5432/d2s7bbg06oe6j4 )
+    $conn = pg_connect("postgres://sldjryctmwqljp:344b41c89cf2851b75d67a9936fce5529a3195de3f5663a5899699d419d70546@ec2-54-226-56-198.compute-1.amazonaws.com:5432/d2s7bbg06oe6j4 ");
             or die("Can not connect database".pg_connect_error());
 ?>
